2421d3702d0be79512336261535557ac0d5b3cee
hoomankhoshdel <hooman.khoshdel@gmail.com>
2023-06-06 11:44:37 +0330

    api-document

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,6 +19,24 @@ from django.views.generic.base import RedirectView
 from django.urls import path,include
 from todo.views import TaskCreate,TaskUpdate,TaskDelete
 from todo import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,5 +49,8 @@ urlpatterns = [
     path('task/',RedirectView.as_view(url = '/tasks')),
     path("register/", views.sign_up, name="register"),
     path("tasks/api/v1/",include('todo.api.v1.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
