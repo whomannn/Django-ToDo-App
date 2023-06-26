e7dbbf0c84d17fe6e4a6f1a3e64afd6c6c8b0172
hoomankhoshdel <hooman.khoshdel@gmail.com>
2023-06-26 07:02:38 +0330

    2

--- core/tests/test_task.py
+++ core/tests/test_task.py
@@ -3,20 +3,38 @@ from django.urls import reverse
 import pytest
 from datetime import datetime
 from django.contrib.auth.models import User
+
+@pytest.fixture
+def common_user():
+    user = User.objects.create_user(email='test2@gmail.com',username="test2",password="test@1234567",is_staff = True)
+    return user
 @pytest.mark.django_db
 class TestTaskApi:
     def test_get_task_response_200(self):
         client = APIClient()
         response = client.get('http://127.0.0.1:8000/tasks/api/v1/task/')
         assert response.status_code == 200
+<<<<<<< Updated upstream
     def test_create_task_response_401(self):
         user = User.objects.create_user(username='testjkbjk',email="test@test.com",password="a/@1234567")
+=======
+    def test_create_task_response_201(self,common_user): 
+>>>>>>> Stashed changes
         client = APIClient()
         data = {
-            'author' : user,
             'title' : 'test',
             'done' : True,
         }
+        client.force_login(user = common_user) 
         response = client.post('http://127.0.0.1:8000/tasks/api/v1/task/',data)
-        assert response.status_code == 401
+        assert response.status_code == 201
+    def test_create_task_invaliddata_response_201(self,common_user): 
+        client = APIClient()
+        data = {
+            
+        }
+        client.force_login(user = common_user) 
+        response = client.post('http://127.0.0.1:8000/tasks/api/v1/task/',data)
+        assert response.status_code == 400
+        
         
