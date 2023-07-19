e51987ca199d0e1f541915ea841f0fa0979e1de6
hoomankhoshdel <hooman.khoshdel@gmail.com>
2023-07-19 14:07:38 +0330

    1

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY", default="test")
 # 'django-insecure-az7ciiklaw383039gn+dei(ub9k@e*k4iju4@ccab@y2!471$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG", cast=bool, default=True)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")], default="*"
